5869fba8ba8b9a0321bafb61ddca76b2afb64329
Ian Kiku <iankiku@gmail.com>
2020-11-17 03:53:13 -0600

    trigger build script

---
+++ run_build.py
@@ -0,0 +1,11 @@
+"""" 
+Trigger Build for a pipeline for diffrent providers 
+"""
+
+
+def run_build(pipeline):
+    """
+   Give it a pipeline and let it run it for you 
+    """
+
+    return pipeline
